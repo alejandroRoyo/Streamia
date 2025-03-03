1eb4818389745a97be5a7b00f1ec7f556292f8ed
alejandroRoyo <alejandro.royo.cortes@gmail.com>
2025-03-03 19:24:27 +0100

    Se añade el odt la imagen para la página principal y se empieza con la creación del componente para los precios

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,6 +1,7 @@
 import { PageProps } from "@/types";
 import { Head, Link } from "@inertiajs/react";
 import Navbar from "@/Pages/Partials/navbar";
+// import TargetaPrecio from "@/Pages/Components/precio";
 
 export default function Welcome({
     auth,
@@ -24,6 +25,21 @@ export default function Welcome({
             <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
                 <Navbar />
             </div>
+            <div className="h-screen bg-[url('/catalogo.avif')] bg-no-repeat bg-center bg-cover flex flex-col justify-center items-center text-center p-5 -mx-5 w-[calc(100%+40px)]">
+                <div className="bg-white/70 p-5 rounded-lg">
+                    <h2 className="text-black font-semibold text-3xl content-center">
+                        Peliculas y series sin límites y mucho más
+                    </h2>
+                    <p>texto de prueba</p>
+                    <a
+                        href="/login"
+                        className="text-white px-5 py-2 rounded-full font-semibold transition duration-200 hover:bg-stone-800"
+                    >
+                        Iniciar Sesión
+                    </a>
+                </div>
+            </div>
+            
         </>
     );
 }
