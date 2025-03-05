d204bc3854ddb93af24f8d368cf69f69c2e16197
alejandroRoyo <alejandro.royo.cortes@gmail.com>
2025-03-05 17:11:33 +0100

    Se intenta registrar usuarios pero sale error 419

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\Auth\RegisterRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+
+class RegisterController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        // Crear el usuario con los datos validados
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        // Iniciar sesión automáticamente con el nuevo usuario
+        Auth::login($user);
+
+        // Redirige al usuario a la página de inicio
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -14,22 +15,16 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/login', function () {
-    return Inertia::render('Auth/login', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::middleware('guest')->group(function () {
+    Route::get('/login', function () {
+        return Inertia::render('Auth/login');
+    })->name('login');
 
-Route::get('/register', function () {
-    return Inertia::render('Auth/register', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    Route::get('/register', function () {
+        return Inertia::render('Auth/register');
+    })->name('register');
+
+    Route::post('/register', [RegisterController::class, 'register']);
 });
 
 Route::middleware('auth')->group(function () {
